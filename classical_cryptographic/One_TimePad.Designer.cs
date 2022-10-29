88d8a0b288ce6444140f88ac8a0c1fc7f89cab83
NganNguyenThiBich <bichngan.bnnt@gmail.com>
2022-10-29 13:10:40 +0700

    one-time-pad

--- classical_cryptographic/One_TimePad.Designer.cs
+++ classical_cryptographic/One_TimePad.Designer.cs
@@ -29,12 +29,129 @@ namespace classical_cryptographic
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lbl_output = new System.Windows.Forms.Label();
+            this.btn_encrypt = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtbox_key = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbl_input = new System.Windows.Forms.Label();
+            this.txtbox_input = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lbl_output
+            // 
+            this.lbl_output.AutoSize = true;
+            this.lbl_output.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_output.Location = new System.Drawing.Point(361, 418);
+            this.lbl_output.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_output.Name = "lbl_output";
+            this.lbl_output.Size = new System.Drawing.Size(0, 29);
+            this.lbl_output.TabIndex = 16;
+            // 
+            // btn_encrypt
+            // 
+            this.btn_encrypt.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_encrypt.Location = new System.Drawing.Point(367, 310);
+            this.btn_encrypt.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_encrypt.Name = "btn_encrypt";
+            this.btn_encrypt.Size = new System.Drawing.Size(133, 54);
+            this.btn_encrypt.TabIndex = 15;
+            this.btn_encrypt.Text = "Encrypt";
+            this.btn_encrypt.UseVisualStyleBackColor = true;
+            this.btn_encrypt.Click += new System.EventHandler(this.btn_encrypt_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(107, 226);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 29);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Key:";
+            // 
+            // txtbox_key
+            // 
+            this.txtbox_key.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtbox_key.Location = new System.Drawing.Point(367, 222);
+            this.txtbox_key.Margin = new System.Windows.Forms.Padding(4);
+            this.txtbox_key.Name = "txtbox_key";
+            this.txtbox_key.Size = new System.Drawing.Size(445, 34);
+            this.txtbox_key.TabIndex = 13;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(265, 54);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(315, 52);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "One Time Pad";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(107, 418);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(195, 29);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Encrypted Text:";
+            // 
+            // lbl_input
+            // 
+            this.lbl_input.AutoSize = true;
+            this.lbl_input.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_input.Location = new System.Drawing.Point(107, 148);
+            this.lbl_input.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_input.Name = "lbl_input";
+            this.lbl_input.Size = new System.Drawing.Size(137, 29);
+            this.lbl_input.TabIndex = 10;
+            this.lbl_input.Text = "Plain Text:";
+            // 
+            // txtbox_input
+            // 
+            this.txtbox_input.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtbox_input.Location = new System.Drawing.Point(367, 148);
+            this.txtbox_input.Margin = new System.Windows.Forms.Padding(4);
+            this.txtbox_input.Name = "txtbox_input";
+            this.txtbox_input.Size = new System.Drawing.Size(445, 34);
+            this.txtbox_input.TabIndex = 9;
+            // 
+            // One_TimePad
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(943, 551);
+            this.Controls.Add(this.lbl_output);
+            this.Controls.Add(this.btn_encrypt);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtbox_key);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbl_input);
+            this.Controls.Add(this.txtbox_input);
+            this.Name = "One_TimePad";
             this.Text = "One_TimePad";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lbl_output;
+        private System.Windows.Forms.Button btn_encrypt;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtbox_key;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbl_input;
+        private System.Windows.Forms.TextBox txtbox_input;
     }
 }
--- classical_cryptographic/One_TimePad.cs
+++ classical_cryptographic/One_TimePad.cs
@@ -16,5 +16,52 @@ namespace classical_cryptographic
         {
             InitializeComponent();
         }
+
+        static string Encipher(string plainText, string key)
+        {
+            string result = "";
+
+            for (int i = 0; i < plainText.Length; i++)
+            {
+                int num = (plainText[i] - 97) + (key[i] - 97);
+                result += (char)((num % 26) + 97);
+            }
+
+            return result;
+        }
+
+        private void btn_encrypt_Click(object sender, EventArgs e)
+        {
+            if (txtbox_key.Text.Length != txtbox_input.Text.Length)
+            {
+                MessageBox.Show("Key has to be the same length as plain text");
+                txtbox_key.Text = "";
+                return;
+            }
+
+            foreach (char ch in txtbox_input.Text)
+            {
+                if (!char.IsLetter(ch))
+                {
+                    MessageBox.Show("Plain text cannot contain numbers or special characters", "Error");
+                    txtbox_input.Text = "";
+                    return;
+                }
+            }
+
+            foreach (char ch in txtbox_key.Text)
+            {
+                if (!char.IsLetter(ch))
+                {
+                    MessageBox.Show("Key cannot contain numbers or special characters", "Error");
+                    txtbox_key.Text = "";
+                    return;
+                }
+
+            }
+
+            lbl_output.Text = Encipher(txtbox_input.Text, txtbox_key.Text);
+
+        }
     }
 }
